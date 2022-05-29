4284d1cd3ff9fa871565524df19fa5a70456d361
ispylnev <ivan_pylnev@mail.ru>
2022-05-29 20:57:09 +0300

    add order service

---
+++ order-service/src/main/java/com/appsdeveloperblog/estore/OrdersService/saga/OrderSaga.java
@@ -0,0 +1,40 @@
+package com.appsdeveloperblog.estore.OrdersService.saga;
+
+import com.appsdeveloperblog.estore.OrdersService.core.events.OrderCreatedEvent;
+import com.communicationcorelibrary.communicationcorelibrary.command.ReserveProductCommand;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.commandhandling.CommandResultMessage;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.spring.stereotype.Saga;
+
+@Saga
+@RequiredArgsConstructor
+public class OrderSaga {
+
+    private transient final CommandGateway commandGateway;
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCreatedEvent orderCreatedEvent) {
+        ReserveProductCommand reserveProductCommand = ReserveProductCommand.builder()
+                .orderID(orderCreatedEvent.getOrderId())
+                .productId(orderCreatedEvent.getProductId())
+                .userId(orderCreatedEvent.getUserId())
+                .quantity(orderCreatedEvent.getQuantity())
+                .build();
+        commandGateway.send(reserveProductCommand, new CommandCallback<ReserveProductCommand, Object>() {
+            @Override
+            public void onResult(CommandMessage<? extends ReserveProductCommand> commandMessage, CommandResultMessage<?> commandResultMessage) {
+                if (commandResultMessage.isExceptional()){
+
+                }
+            }
+        });
+    }
+
+
+}
